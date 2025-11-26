507a9b2245ff87543d2c9d8d9c4a1f363c32f241
wisetree911 <gariksharik224@gmail.com>
2025-11-27 02:50:07 +0500

    cleaning

--- backend/app/services/analytics.py
+++ backend/app/services/analytics.py
@@ -7,31 +7,29 @@ class AnalyticsService:
     @staticmethod
     async def portfolio_dynamics(session, user_id: int):
         portfolios = await PortfolioRepository.get_by_user_id(session=session, user_id=user_id)
-
+        since = datetime.utcnow() - timedelta(hours=24)
         response = []
         for portfolio in portfolios:
             positions = await PortfolioPositionRepository.get_by_portfolio_id(session=session, portfolio_id=portfolio.id)
+            if not positions:
+                response.append({"id": portfolio.id, "name": portfolio.name, "data": []})
+                continue
 
             asset_ids = [p.asset_id for p in positions]
             pos_dict = {p.asset_id: p.quantity for p in positions}
 
-            since = datetime.utcnow() - timedelta(hours=24)
             prices = await AssetPriceRepository.get_prices_since(session=session, ids=asset_ids, since=since)
-            time_map = {}
 
+            time_map = {}
             for price in prices:
                 ts = price.timestamp
                 asset_id = price.asset_id
                 if ts not in time_map: time_map[ts] = 0
                 time_map[ts] += price.price * pos_dict[asset_id]
             
-            data = [
-                    {
-                        "timestamp": ts.isoformat(),
-                        "value": float(val)
-                    }
+            data = [{"timestamp": ts.isoformat(), "value": float(val)}
                     for ts, val in sorted(time_map.items(), key=lambda x: x[0])
-                ]
+            ]
             
             response.append({
                 "id" : portfolio.id,
