c511c0098ca2aacab9147f42104a4f432525dbb1
wisetree911 <gariksharik224@gmail.com>
2025-12-01 06:39:57 +0500

    added descriptions for schemas/analytics.py

--- backend/app/schemas/analytics.py
+++ backend/app/schemas/analytics.py
@@ -3,29 +3,24 @@ from datetime import datetime
 from typing import List
 
 class TopPosition(BaseModel):
-    asset_id: int
-    ticker: str
-    full_name: str
-    quantity: int
-    avg_buy_price: float
-    current_price: float
-    current_value: float
-    profit: float
-    profit_percent: float
+    asset_id: int = Field(..., description="asset ID")
+    ticker: str = Field(..., description="asset ticker, for example: GAZP")
+    full_name: str= Field(..., description="full name of asset")
+    quantity: int= Field(..., description="quantity of asset")
+    avg_buy_price: float= Field(..., description="average buy price of asset")
+    current_price: float= Field(..., description="current price of asset")
+    current_value: float= Field(..., description="current value of position (quantity * current_price)")
+    profit: float= Field(..., description="absolute profit of asset")
+    profit_percent: float= Field(..., description="profit of asset in percents")
 
 
 class PortfolioShapshotResponse(BaseModel):
-    portfolio_id: int
-    name: str
-
-    total_value: float
-    total_profit: float
-    total_profit_percent: float
-
-    invested_value: float
-
-    currency: str
-
-    positions_count: int
-
-    top_positions: List[TopPosition] | None = None
+    portfolio_id: int= Field(..., description="portfolio ID")
+    name: str= Field(..., description="portfolio name")
+    total_value: float= Field(..., description="current value of portfolio")
+    total_profit: float= Field(..., description="current profit of portfolio")
+    total_profit_percent: float= Field(..., description="current porfit of portfolio in percents")
+    invested_value: float= Field(..., description="value invested in portfolio initially")
+    currency: str= Field(..., description="currency of portfolio, for example: RUB")
+    positions_count: int= Field(..., description="number of unique assets in portfolio")
+    top_positions: List[TopPosition] = Field(..., description="top 3 positions in portfolio by value part in portfolio")
