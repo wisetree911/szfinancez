8e8407671aecd898355b2de60020bcff702e8931
wisetree911 <gariksharik224@gmail.com>
2025-12-02 21:20:11 +0500

    refactored backend/price_updater/services/service.py as a class PriceService, decomposed backend/price_updater/services/service.py:update_prices functionality, moved all aiohttp to backend/price_updater/clients/moex_client.py, refactored prices updating logic, now using 1 atomary transaction instead of 200 separated transactions later

--- backend/price_updater/clients/moex_client.py
+++ backend/price_updater/clients/moex_client.py
@@ -2,25 +2,30 @@ import aiohttp
 from loguru import logger
 
 class MoexClient:
-    def __init__(self, session: aiohttp.ClientSession):
-        self.session = session
     
-    async def get_all_prices(self) -> dict:
-        url = f"https://iss.moex.com/iss/engines/stock/markets/shares/securities.json?marketdata.columns=SECID,LAST"
-        try:
-            async with self.session.get(url, timeout=30) as resp:
-                data = await resp.json()
-        except Exception as e:
-            logger.error(f"!!!!!! Ошибка сети: {e} !!!!!")
-            raise
-        prices = {}
-        for row in data["marketdata"]["data"]:
-            prices[row[0]] = row[1]
-        try:
-            return prices
-        except Exception:
-            logger.error(f" !!!!!!! Ошибка парсинга ответа: {data} !!!!!!!")
-            raise
+    @staticmethod
+    async def get_all_prices() -> dict:
+        async with aiohttp.ClientSession(connector=aiohttp.connector.TCPConnector(ssl=False)) as session:
+            url = f"https://iss.moex.com/iss/engines/stock/markets/shares/securities.json?marketdata.columns=SECID,LAST"
+            try:
+                async with session.get(url, timeout=30) as resp:
+                    data = await resp.json()
+            except Exception as e:
+                logger.error(f"!!!!!! Ошибка сети: {e} !!!!!")
+                raise
+            prices = {}
+            for row in data["marketdata"]["data"]:
+                prices[row[0]] = row[1]
+            try:
+                return prices
+            except Exception:
+                logger.error(f" !!!!!!! Ошибка парсинга ответа: {data} !!!!!!!")
+                raise
+
+
+
+
+
 
 
     async def get_security_info(self, ticker: str) -> dict:
--- backend/price_updater/services/service.py
+++ backend/price_updater/services/service.py
@@ -1,44 +1,29 @@
 from loguru import logger
 import aiohttp
-from app.core.database import get_session
 from price_updater.clients.moex_client import MoexClient
 from shared.repositories.asset_price import AssetPriceRepository
 from app.schemas.asset_price import AssetPriceCreate
 from app.core.database import async_session_maker
-async def fetch_prices(moex: MoexClient):
-    try:
-        prices = await moex.get_all_prices()
-        return prices
-    except Exception as e:
-        logger.error(f"!!!!!!! Ошибка при получении: {e} !!!!!!!")
-        return None
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def update_prices(session, asset_registry):
-    logger.info("****** Обновление цен *******")
+class PricesService():
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        self.repo = AssetPriceRepository
 
-    assets = asset_registry.get_all()
-    if not assets:
-        logger.warning("!!!!!! Нет активов для обновления !!!!!!")
-        return
-    connector = aiohttp.connector.TCPConnector(ssl=False)
-    async with aiohttp.ClientSession(connector=connector) as http_session:
-        moex = MoexClient(session=http_session)
-        try:
-            prices = await moex.get_all_prices()
-        except Exception as e:
-            logger.error(f"!!!!!!! Ошибка при получении цен: {e} !!!!!!!")
-            return
-
-    for asset_id, ticker in assets.items():
-        price = prices.get(ticker)
-        if price is None:
-            logger.warning(f"⚠ Нет цены для тикера {ticker}")
-            continue
+    async def update_prices(self, asset_registry):
+        logger.info("****** Обновление цен *******")
+        assets = asset_registry.get_all()
+        if not assets: return
+        prices = await MoexClient.get_all_prices()
         async with async_session_maker() as session:
-            repo = AssetPriceRepository(session=session)
-            await repo.create(AssetPriceCreate(asset_id=asset_id, price=price, currency="RUB", source="moex"))
-            logger.info(f"💰 {ticker}: {price}")
+            async with session.begin():
+                repo = self.repo(session=session)
+                for asset_id, ticker in assets.items():
+                    price = prices.get(ticker)
+                    if price is None: continue
+                    await repo.create(AssetPriceCreate(asset_id=asset_id, price=price, currency="RUB", source="moex"))
+                    logger.info(f"💰 {ticker}: {price}")
 
-    await session.commit()
-    logger.info("****** Обновление завершено ******")
+        logger.info("****** Обновление завершено ******")
